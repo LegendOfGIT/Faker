461fbb9a1fecb6d14c055da0120e56aa8d3958c7
marcel schneider <marcel.marschner@arvato.com>
2018-06-26 16:54:56 +0200

    - added first test

---
+++ tests/Faker/FakerFactoryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Faker;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests of faker factory
+ */
+class FakerFactoryTest extends TestCase
+{
+    public function testFirstTest()
+    {
+        $this->assertTrue(true);
+    }
+}
